35acf385958dc1320ec108aeb25ae5b053242a59
Kostya Gavrilchenko <12fthofmay@gmail.com>
2013-11-03 23:43:24 +0200

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,29 @@
-for folder in $(find . -mindepth 1 -maxdepth 1 -type d -name '*Portlet' -printf "%P\n") ; do
-    echo $folder; 
+echo ""
+$LIFERAY_TOMCAT/bin/shutdown.sh
+sleep 30
+
+cd "$LIFERAY_TOMCAT/webapps/"
+#remove all stud- portlets from webapps
+for folder in $(find . -mindepth 1 -maxdepth 1 -type d -name 'stud-*' -printf "%P\n") ; do
+    echo rm -r $folder; 
 done
 
-cd $LIFERAY_TOMCAT/webapps/
+#clean liferay`s deploy directory and assign path to this directory to "directory_to_deploy" variable
+cd $LIFERAY_TOMCAT
+cd ../deploy
 
-for folder in $(find . -mindepth 1 -maxdepth 1 -type d -name 'stud-*' -printf "%P\n") ; do
-    echo $folder; 
+echo $(pwd)
+directory_to_deploy=$(pwd)
+
+for smth in $(find . -mindepth 1 -maxdepth 1 -name '*.war' -printf "%P\n") ; do
+    rm $smth;
+done
+#$BUILD_ID = 'dontKillMe'
+$LIFERAY_TOMCAT/bin/startup.sh
+sleep 90
+cd $WORKSPACE
+#copy all war files to liferay`s deploy directory
+for folder in $(find . -mindepth 1 -maxdepth 1 -type d -name '*Portlet' -printf "%P\n") ; do
+    cp $folder/target/*.war $directory_to_deploy ; 
+    sleep 60;
 done
